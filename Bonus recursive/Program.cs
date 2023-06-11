50bf5f41062d4fe2ef34be4ee45427c26e3a633a
Ker Qian Ning <kerqianning2005@gmail.com>
2023-06-11 21:38:15 +0800

    Everything

---
+++ Bonus recursive/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {   
+        int num;
+        while (true) {
+            Console.WriteLine("Enter a number (1 or less to exit): ");
+            if (int.TryParse(Console.ReadLine(), out num)) {
+                if (num <= 1) {
+                    break;
+                } else {
+                    Console.WriteLine((isPrime(num))? "Prime" : "Not Prime");
+                }
+            } else {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+    static bool isPrime(int num, int divisor = 2)
+    {
+        if (num == divisor) {
+            //Prime numbers are only divisable by 1 and themselves;
+            return true;
+        } else if (num % divisor == 0) {
+            //If the number is divisable by any number other than 1 or itself, it is not prime
+            return false;
+        } else {
+            //If the number is not divisable by the current divisor, try the next one
+            return isPrime(num, divisor + 1);
+        }
+    }
+}
---
+++ Prime Checker/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Check_Prime;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        while (true) {
+            Console.WriteLine("Enter a number (1 or less to exit): ");
+            if (int.TryParse(Console.ReadLine(), out int num) && num > 1) {
+                Console.WriteLine(isPrime(num));
+            } else {
+                break;
+            }
+        }
+    }
+    static bool isPrime(int num) {
+        for (int i = 2; i < num ; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
